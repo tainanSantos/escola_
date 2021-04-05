e931057e029440543a820455f04d3d3f84a84527
Tainan Santos <tainan206@gmail.com>
2021-04-05 14:46:17 -0300

    sobrescrevo métodos get_object() e get_queryset() das avaliações e contruido rotas especificas para receber informações retornadas pelos métodos subscritos

--- cursos/urls.py
+++ cursos/urls.py
@@ -4,7 +4,18 @@ from .views import CursoAPIView, AvaliacaoAPIView, AvaliacoesAPIView, CursosAPIV
 
 urlpatterns = [
     path('cursos/', CursosAPIView.as_view(), name='cursos'),
-    path('avaliacoes/', AvaliacoesAPIView.as_view(), name='avaliacoes'),
+    # não foi colocado curso_pk pois não iremos sobrescrever nenhum método dessa parte
     path('cursos/<int:pk>/', CursoAPIView.as_view(), name='curso'),
-    path('avaliacoes/<int:pk>/', AvaliacaoAPIView.as_view(), name='avaliacao')
+
+    # rota que vai pegar um curso e trazer todas as avaliações daquele curso
+    # curso_pk é só par asaber que o id é do curso e não da avaliação
+    path('cursos/<int:curso_pk>/avaliacoes', AvaliacoesAPIView.as_view(), name='curso_avaliacoes'),
+    # rota que vai pegar um curso e trazer todas as avaliações daquele curso
+    # curso_pk é só par asaber que o id é do curso e não da avaliação
+    # e depois uma avaliação específica daquele curso selecionado
+    path('cursos/<int:curso_pk>/avaliacoes/<int:avaliacao_pk>', AvaliacaoAPIView.as_view(), name='curso_avaliacao'),
+
+
+    path('avaliacoes/', AvaliacoesAPIView.as_view(), name='avaliacoes'),
+    path('avaliacoes/<int:avaliacao_pk>/', AvaliacaoAPIView.as_view(), name='avaliacao')
 ]
--- cursos/views.py
+++ cursos/views.py
@@ -1,7 +1,5 @@
 from rest_framework import generics
-from rest_framework.response import Response
-# status de respostas da api
-from rest_framework import status
+from rest_framework.generics import get_object_or_404
 
 from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
@@ -22,17 +20,37 @@ class CursosAPIView(generics.ListCreateAPIView):
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
 
+
 # buscar um, editar um e deletar um
 class CursoAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
+
 
 # criar e buscar todos
 class AvaliacoesAPIView(generics.ListCreateAPIView):
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
 
+    # sobrescrevendo método get_queryset
+    # o metodo get_queryset retorna uam lista
+    # pegando todas as avaliações de um determinado curso
+    def get_queryset(self):
+        if self.kwargs.get('curso_pk'):  # self.kwargs.get('curso_pk') verificando se veio algum curso_pk na url
+
+            return self.queryset.filter(curso_id=self.kwargs.get('curso_pk'))  # retornando todas as avaliaçõe do curso
+        return self.queryset.all()  # retornando todas as avaliações
+
+
 # buscar um, editar um e deletar um
 class AvaliacaoAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
+
+    # o método get_object retorna uma lista
+    # pegando uam avaliação específica do curso selecionado
+    def get_object(self):
+        if self.kwargs.get('curso_pk'):
+            return get_object_or_404(self.get_queryset(), curso_id=self.kwargs.get('curso_pk'),
+                                     pk=self.kwargs.get('avaliacao_pk'))
+        return get_object_or_404(self.get_queryset(), pk=self.kwargs.get('avaliacao_pk'))

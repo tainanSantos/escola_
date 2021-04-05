9997b22fbe3e1de69ac8b586e9ae1a7c5e6e8c51
Tainan Santos <tainan206@gmail.com>
2021-04-05 15:32:23 -0300

    customizado as viewSet com  mixins

--- cursos/views.py
+++ cursos/views.py
@@ -5,6 +5,7 @@ from rest_framework.generics import get_object_or_404
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework import mixins
 
 from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
@@ -27,8 +28,6 @@ API V1
 class CursosAPIView(generics.ListCreateAPIView):
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
-
-
 
 
 # buscar um, editar um e deletar um
@@ -86,6 +85,22 @@ class CursoViewSet(viewsets.ModelViewSet):
         return Response(serializer.data)
 
 
+"""
 class AvaliacaoViewSet(viewsets.ModelViewSet):
+    queryset = Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer
+"""
+
+# faz a mesma coisa da class comentada acima
+# o que muda é que se desejar excluir algum método do crud
+# é so excluir uma atribito de mixins
+class AvaliacaoViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    # mixins.DestroyModelMixin,
+    viewsets.GenericViewSet
+):
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer

f8ad451ddc28b9f42ab3b03a8cbbd67a59b38638
Tainan Santos <tainan206@gmail.com>
2021-04-05 13:59:39 -0300

    modificamso o pkg APIView para utilizar o pkg GenericView do rest_framework

--- cursos/views.py
+++ cursos/views.py
@@ -1,4 +1,4 @@
-from rest_framework.views import APIView
+from rest_framework import generics
 from rest_framework.response import Response
 # status de respostas da api
 from rest_framework import status
@@ -7,46 +7,32 @@ from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
 
 
-class CursoAPIView(APIView):
-    """
-    API de Curso da Geek
-    """
-
-    def get(self, request):
-        print("INFORMAÇÕES DO REQUEST")
-        print(dir(request))
-        print(request.query_params)
-        print(request.user)
-
-        cursos = Curso.objects.all()
-        serializer = CursoSerializer(cursos, many=True)
-        return Response(serializer.data)
+# class CursoAPIView(generics.ListCreateAPIView):
+#     queryset = Curso.objects.all()
+#     serializer_class = CursoSerializer
+#
+#
+# class AvaliacaoAPIView(generics.ListCreateAPIView):
+#     queryset = Avaliacao.objects.all()
+#     serializer_class = AvaliacaoSerializer
 
 
-    def post(self, request):
-        serializer = CursoSerializer(data=request.data)
-        # se os dados forem inválidos ele já para aqui e lança uma exception
-        serializer.is_valid(raise_exception=True)
-        serializer.save() # salvando do dados no BD
-        return Response(serializer.data, status =status.HTTP_201_CREATED)
-        # return Response({"id":serializer.data['id'], "curso":serializer.data['titulo']}, status =status.HTTP_201_CREATED)
-        # return Response({"msg":"Curso criado com sucesso"}, status =status.HTTP_201_CREATED)
+# criar e buscar todos
+class CursosAPIView(generics.ListCreateAPIView):
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
 
-class AvaliacaoAPIView(APIView):
-    """
-    Api de Avaliações da Geek
-    """
+# buscar um, editar um e deletar um
+class CursoAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
 
-    def get(self, request):
-        avaliacoes = Avaliacao.objects.all()
-        # many=True quer dizer qu eeu quero buscar todas as avaliações
-        serializer = AvaliacaoSerializer(avaliacoes, many=True)
-        return Response(serializer.data)
+# criar e buscar todos
+class AvaliacoesAPIView(generics.ListCreateAPIView):
+    queryset = Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer
 
-    def post(self, request):
-        serializer = AvaliacaoSerializer(data=request.data)
-        # se os dados forem inválidos ele já para aqui e lança uma exception
-        serializer.is_valid(raise_exception=True)
-        serializer.save() # salvando do dados no BD
-        return Response(serializer.data, status =status.HTTP_201_CREATED)
-
+# buscar um, editar um e deletar um
+class AvaliacaoAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer
